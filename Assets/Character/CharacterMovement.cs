7f8e0e89346fa70f0f8ee0335ebdeeb384910d9d
Nathan <nathanaelkinnis@hotmail.com>
2020-10-17 16:18:54 +0300

    Player can walk and look around

--- Assets/Character/CharacterMovement.cs
+++ Assets/Character/CharacterMovement.cs
@@ -15,7 +15,7 @@ public class CharacterMovement : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * x * transform.forward * z;
+        Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
     }
