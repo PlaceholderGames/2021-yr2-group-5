370055e42538f2c2942f1aae55ff0654e1cc03af
Nathan <nathanaelkinnis@hotmail.com>
2020-10-22 00:31:25 +0300

    Made the progress bar for the transormation, which for now just ends the game. I used some simple art i found online which we can change later

---
+++ Assets/Character/ProgressBar/BarCode.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarCode : MonoBehaviour
+{
+    Image timerBar;
+    public float maxTime=5f;
+    float timeLeft;
+  
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    timerBar = GetComponent<Image>();
+    timeLeft = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeLeft < maxTime)
+        {
+            timeLeft += Time.deltaTime;
+            timerBar.fillAmount = timeLeft / maxTime;
+        }
+    else
+        {
+            Time.timeScale = 0;
+        }
+    }
+}
