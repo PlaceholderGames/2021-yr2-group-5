d39cce795f9bf1c5b2281ece03967d2eed52ed35
Nathan <nathanaelkinnis@hotmail.com>
2020-10-23 17:20:55 +0300

    Player can now pick up potions ( currently in the form of sphere, and it will show on the screen the amount of potions that the player has, the amount of potions the player can carry is capped at 1 for now

---
+++ Assets/Character/PlayerManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    public bool HavePotion = false;
+
+    //public int Potion;
+    public int numOfPotion=0;
+    public Image[] potions;
+    public Sprite potionImage;
+
+    public void PickupItem()
+    {
+        HavePotion = true;
+        numOfPotion++;
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < potions.Length; i++)
+        {
+            if (i < numOfPotion)
+            {
+                potions[i].enabled = true;
+            }
+            else
+            {
+                potions[i].enabled = false;
+            }
+        }
+        
+    }
+}
---
+++ Assets/Potion/PickUp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUp : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider collision)
+    {
+        PlayerManager manager = collision.GetComponent<PlayerManager>();
+        if (manager)
+        {
+            if (manager.HavePotion == false)
+            { 
+                manager.PickupItem();
+                Destroy(gameObject);
+            }
+        }
+    }
+}
