49d8d997b69011b60e0d0ceef6d85af752b8a7d1
Nathan <nathanaelkinnis@hotmail.com>
2020-10-30 15:00:43 +0200

    Pick up and collision system works now with no errors, next up going to add a menu and an end screen

--- Assets/Potion/PickUp.cs
+++ Assets/Potion/PickUp.cs
@@ -1,25 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 public class PickUp : MonoBehaviour
 {
     public GameObject _Text;
-    PlayerManager manager;
+    
     //private PotionTextScript Pscript;
     private void OnTriggerEnter(Collider collision)
     {   
         _Text.SetActive(true);   
     }
 
-    void OnCollisionStay(Collision collision)
+    private void OnTriggerStay(Collider collision)
     {
-        print("stuff");
-        if (Input.GetKeyDown("f"))
+        PlayerManager manager = collision.GetComponent<PlayerManager>();
+        print("oi");
+        if ((manager.HavePotion == false) && (Input.GetKeyDown("f")))
         {
-            print("fuck");
             manager.PickupItem();
+            print("nai");
             Destroy(gameObject);
         }
+        
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        _Text.SetActive(false);
     }
 }
