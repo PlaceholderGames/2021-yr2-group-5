56ac03cee766bea0b86328afb86234b2583a56d0
Nathan <nathanaelkinnis@hotmail.com>
2020-11-05 20:07:08 +0200

    Added an enemy that chases the player around, will make some improvements and will at spawn points next

---
+++ Assets/Enemy/EnemyAI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    private NavMeshAgent demon;
+
+    public GameObject Player;
+
+    public float demonDistanceRun = 4.0f;
+
+    void Start()
+    {
+        demon = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        float distance = Vector3.Distance(transform.position, Player.transform.position);
+
+        if(distance<demonDistanceRun)
+        {
+            Vector3 dirToPlayer = transform.position - Player.transform.position;
+
+            Vector3 newPos = transform.position - dirToPlayer;
+
+            demon.SetDestination(newPos);
+        }
+    }
+}
