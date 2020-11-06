cada1afac349719d6454f1032dcbadcd728ccc39
Nathan <nathanaelkinnis@hotmail.com>
2020-11-06 05:47:35 +0200

    Added spawners but enemies are not moving towards the player as intended, will fix asap

---
+++ Assets/Enemy/EnemySpawners.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using UnityEngine;
+
+public class EnemySpawners : MonoBehaviour
+{
+    public Transform[] _SpawnPoints;
+    public GameObject _DemonPrefab;
+    public BarCode _bar;
+    private bool spawning=true;
+
+    void Start()
+    {
+        SpawnNewDemon();
+    }
+
+    void Update()
+    {
+        int num = (int)_bar.timeLeft;
+        if ((num % 10) !=0)
+        {
+            spawning = true;
+        }
+        if (num % 10 == 0 && num != 0 && spawning == true)
+        {
+            SpawnNewDemon();
+            spawning = false;
+        }
+    }
+
+    void SpawnNewDemon()
+    {
+        int randomNumber = Mathf.RoundToInt(Random.Range(0f, _SpawnPoints.Length - 1));
+        
+        Instantiate(_DemonPrefab, _SpawnPoints[randomNumber].transform.position/*, Quaternion.identity*/);
+        print(randomNumber);
+    }
+}
