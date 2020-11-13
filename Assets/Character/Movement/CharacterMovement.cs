07c24a9c783ee34873e728dfe1373c021ad0c9bc
Nathan <nathanaelkinnis@hotmail.com>
2020-11-13 02:02:25 +0200

    Added a crouch action with left control and player now has a flaslight which he can toggle on and off with q, keys bindings can be changed its just temp ones

--- Assets/Character/Movement/CharacterMovement.cs
+++ Assets/Character/Movement/CharacterMovement.cs
@@ -6,21 +6,42 @@ using UnityEngine;
 public class CharacterMovement : MonoBehaviour
 {
     public CharacterController controller;
+    Flashlight _flash;
     public float walkSpeed = 10f;
     public float sprintSpeed = 15f;
-    public Vector3 position;
+    public float crouchSpeed = 5f;
+    public Vector3 position; 
 
     // Update is called once per frame
     void Update()
     {
-        float currentSpeed = walkSpeed;
-        float x = Input.GetAxis("Horizontal");
+        float currentSpeed;  
+        float x = Input.GetAxis("Horizontal"); 
         float z = Input.GetAxis("Vertical");
 
         if(Input.GetKey(KeyCode.LeftShift) && z > 0)
         {
+            if(controller.height < 3.8)
+            {
+                controller.height += 0.1f;
+            }
             currentSpeed = sprintSpeed;
-        }
+            
+        } // Check if sprinting
+        else if (Input.GetKey(KeyCode.LeftControl))
+        {
+            currentSpeed = crouchSpeed;
+            controller.height = 2f;
+        } //Check if crouching
+        else 
+        {
+            if (controller.height < 3.8)
+            {
+                controller.height+=0.1f;
+            }
+            currentSpeed = walkSpeed;
+            
+        } //If walking
 
         Vector3 move = transform.right * x + transform.forward * z + transform.up*-1;
         controller.Move(move * currentSpeed * Time.deltaTime);
