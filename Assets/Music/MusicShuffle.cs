38b742072263036a95fb10a19fc64718dbcc0cbd
Nathan <nathanaelkinnis@hotmail.com>
2020-12-27 05:29:40 +0200

    Game now has some background music playing, i added 3 random clips i found online, they should be changed/have more music added so we dont have to repeat the same ones more than once

---
+++ Assets/Music/MusicShuffle.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class MusicShuffle : MonoBehaviour
+{
+    public AudioSource Music1;
+    public AudioSource Music2;
+    public AudioSource Music3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Music1 = gameObject.AddComponent<AudioSource>();
+        //Music2 = gameObject.AddComponent<AudioSource>();
+        //Music3 = gameObject.AddComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (!Music1.isPlaying && !Music2.isPlaying && !Music3.isPlaying)
+        {
+            
+            int num = Random.Range(1, 4);
+            
+            switch (num)
+            {
+                case 1:
+                    print(num);
+                    Music1.Play();
+                    break;
+                case 2:
+                    print(num);
+                    Music2.Play();
+                    break;
+                case 3:
+                    print(num);
+                    Music3.Play();
+                    break;
+                case 4:
+                    print(num);
+                    break;
+            }
+                    
+        }
+            
+    }
+}
