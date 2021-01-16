b8eaa77e507604294249259e25296d63d01d3e1a
Nathan <nathanaelkinnis@hotmail.com>
2021-01-16 16:53:04 +0200

    Added a button when the player gets the notes that closes the note, made the cursor visible to when a note is active and tried to disable the movement and looking around scripts, so when the player has a note open, he wont be able to move or look around, but doesnt work properly thus far

--- Assets/Items/Notes/noteManager.cs
+++ Assets/Items/Notes/noteManager.cs
@@ -6,25 +6,34 @@ using UnityEngine.UI;
 public class noteManager : MonoBehaviour
 {
     public GameObject _noteImage;
-    //bool activeImage;
+    //public CharacterMovement _mov;
+    //public MouseLook _look;
+   // var _moveScript;
 
     // Start is called before the first frame update
     void Start()
     {
-         _noteImage.SetActive(false);
+        _noteImage.SetActive(false);
     }
 
     public void openImage()
     {
+       // var _moveScript = gameObject.GetComponent(CharacterMovement);
         _noteImage.SetActive(true);
         Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        //_moveScript.enabled = false;
+
+        print("open");
         //store the image in array here before delete
-        // activeImage = false;
+
     }
 
     public void closeImage()
     {
         _noteImage.SetActive(false);
+        print("false");
         Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
